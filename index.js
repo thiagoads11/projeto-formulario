6c9f825f829ecf43124b561b4be3aeb17568d8f2
Thiago Menezes <thmenezesdev@gmail.com>
2024-04-09 15:48:24 -0300

    add new changes in index.js

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require("express")
 
 const app = express()
 
-const users = {
+app.use(express.json())
+
+const users = [
   {
     name: 'Thiago',
     age: 29,
   },
-}
+]
+
+app.get('/usuarios', function (request,response){
+  response.json(users)
+})
+
+app.post('/usuarios', function (request,response){
+  console.log(request.body)
+
+  const newUser = resquest.body
+
+  users.push(newUser)
+
+  response.status(201).json(newUser)
+  
+})
 
 
 app.listen(3001, () => console.log('Servidor Rodando'))
