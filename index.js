b849066ca14438fb2e83b7ef438654ab96e3557f
Thiago Menezes <thmenezesdev@gmail.com>
2024-04-10 02:12:06 -0300

    add new changes, add cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const users = [
   {
     name: 'Thiago',
-    age: 29,
+    email: 'thiagomenezes@gmail.com',
   },
 ]
 
